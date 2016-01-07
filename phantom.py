6035ff41592c93dcdb7580aef6f2f717a4e079e8
杜耀光 <duyaoguang@6clue.com>
2016-01-07 17:36:14 +0800

    fix bugs

--- phantom.py
+++
@@ -1,6 +0,0 @@
-HOSTS = [
-            "http://localhost:8888"
-]
-TINYURL = { 'tinyurl':'http://jiyuapi.here.cn/tinyurl?url='}
-UPLOADING = {'url':'http://img.here.cn/uploadimg'}
-DB={'address':'192.168.100.186:20301,192.168.100.133:20301','db':'weixin','col':'article','replicaSet':'dmmongo'}
