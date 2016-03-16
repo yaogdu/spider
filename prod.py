1891d94d672d432c7f58b68e2837560dfe1cbf5d
yaogdu <toby_du@126.com>
2016-03-16 18:07:32 +0800

    Update prod.py

--- prod.py
+++ prod.py
@@ -1,3 +1,3 @@
-TINYURL = { 'tinyurl':'http://here.cn/?url='}
-UPLOADING = {'url':'http://img.demai.com/uploadimg'}
-DB={'address':'172.16.0.119:20301,172.16.0.130:20301','db':'weixin','col':'article','replicaSet':'dmmongo'}
+TINYURL = { 'tinyurl':'www.baidu.com'}
+UPLOADING = {'url':'http://www.baidu.com'}
+DB={'address':'111,1111','db':'111','111':'111','replicaSet':'dmmongo'}
